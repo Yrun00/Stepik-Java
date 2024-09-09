c145b814d1c70a6ca18d9b27e0852593a10b85ca
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-09-09 20:33:17 +0400

    grades

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,14 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int dayNumber = scan.nextInt();
-        String day;
-        switch (dayNumber) {
-            case 1, 2, 3, 4, 5 -> day = "Working day";
-            case 6 -> day = "Saturday";
-            case 7 -> day = "Sunday";
-            default -> day = "ERROR";
-
+        int grade = scan.nextInt();
+        int bads = 0;
+        for (int i = 0; i < 9; i++) {
+            if (grade<4){
+                bads++;
+            }
+            grade = scan.nextInt();
         }
-        System.out.println(day);
+        System.out.println(bads);
     }
 }
