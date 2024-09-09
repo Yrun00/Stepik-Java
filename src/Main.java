8546e61c33980d20ae042b98b616831d5230ee07
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-09-09 21:37:51 +0400

    length and sum of digits

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,18 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        long digit = scan.nextLong();
+        long number = Math.abs(scan.nextLong());
+        int length = Long.toString(number).length();
         long sum = 0;
-        int dilimeter;
+        int i = 0;
+        long delimiter = (long) Math.pow(10, length);
         do {
-
-            dilimeter = scan.nextInt();
-            if (dilimeter < 0) continue;
-            if (digit % dilimeter == 0) sum++;
-        } while (dilimeter > 0);
-        System.out.println(sum);
+            sum = sum + number / delimiter;
+            number = number % delimiter;
+            delimiter = delimiter / 10;
+            i++;
+        } while (i <= length
+        );
+        System.out.println(length + " " + sum);
     }
 }
