3cdb69559908a0571612b8991936dd1e55ca31e6
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-09-10 01:52:53 +0400

    first max sum number from range

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
-        if (number < 1) {
-            System.out.print("ERROR");
-            return;
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int number = Integer.MIN_VALUE;
+        int answer = Integer.MIN_VALUE;
+        if (a > b) {
+            int c = a;
+            a = b;
+            b = c;
         }
-        for (int i = 1; i <= number; i++) {
-
-            for (int a = 1; a <= number; a++) {
-                System.out.printf(("%d\t"), i * a);
-
+        for (; a <= b; a++) {
+            int length = Integer.toString(Math.abs(a)).length();
+            int temporary = Math.abs(a);
+            int sum = 0;
+            for (int i = 1; i <= length; i++) {
+                sum = (sum + temporary / (int) Math.pow(10, (length - i)));
+                temporary = temporary % (int) Math.pow(10, (length - i));
+                if (sum > number) {
+                    answer = a;
+                    number = sum;
+                }
             }
-            System.out.println();
         }
+        System.out.print(answer);
     }
 }
