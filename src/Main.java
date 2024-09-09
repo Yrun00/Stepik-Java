186d0442966105e83aae3898b7f8c031b3ea209d
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-09-09 17:12:40 +0400

    salary

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,38 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<>();
-        String line = scan.nextLine();
-        scan.close();
-        scan = new Scanner(line);
-        while (scan.hasNextInt()) {
-            int number = scan.nextInt();
-            list.add(number);
-        } ;
-        int min = list.get(0);
-        for (Integer integer : list) {
-            if (min > integer) min = integer;
+        int hours = scan.nextInt();
+        float salary = scan.nextFloat();
+        if (hours < 0 | salary < 0) {
+            System.out.println("ERROR");
+            return;
         }
-        System.out.println(min);
+        float pay = 0;
+        int iq = 0;
+        if (hours > 0 && hours <= 20) {
+            iq = 1;
+        } else if (hours > 20 && hours <= 40) {
+            iq = 2;
+        } else if (hours > 40) {
+            iq = 3;
+        }
+        switch (iq) {
+            case (1):
+                pay = hours * salary;
+                break;
+            case (2):
+                pay = 20 * salary+(hours-20)*salary*1.5F;
+                break;
+            case (3):
+                pay = 20 * salary+(20)*salary*1.5F+(hours-40)*salary*2;
+                break;
+            default:
+                break;
+        }
+
+        System.out.printf("%5.2f\n", pay);
+
     }
 }
