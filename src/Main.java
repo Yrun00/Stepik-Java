3cc44dccb97e4153abcabf889db05ce47ca1c04e
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-09-10 00:52:00 +0400

    triangle

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,15 @@ public class Main {
             System.out.print("ERROR");
             return;
         }
-        for (int i = 0; i < number; i++) {
+        for (int i = 0; i < number; i = i + 2) {
+            int z = i;
+            while ((z - 2 >= 0)) {
+                z = z - 2;
+                System.out.print(" ");
+            }
             for (int a = 1; a <= number; a++) {
+
                 if (i < a) System.out.print("*");
-                else System.out.print(" ");
             }
             System.out.println();
         }
