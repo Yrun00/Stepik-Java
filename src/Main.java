a1f98df82bdeb8085ab1d8357e6f6d7e1986e471
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-09-09 19:45:25 +0400

    days

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,12 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String day = scan.next().toUpperCase();
-        switch (day) {
-            case "ПОНЕДЕЛЬНИК" -> day = "Monday";
-            case "ВТОРНИК" -> day = "Tuesday";
-            case "СРЕДА" -> day = "Wednesday";
-            case "ЧЕТВЕРГ" -> day = "Thursday";
-            case "ПЯТНИЦА" -> day = "Friday ";
-            case "СУББОТА" -> day = "Saturday";
-            case "ВОСКРЕСЕНЬЕ" -> day = "Sunday";
+        int dayNumber = scan.nextInt();
+        String day;
+        switch (dayNumber) {
+            case 1, 2, 3, 4, 5 -> day = "Working day";
+            case 6 -> day = "Saturday";
+            case 7 -> day = "Sunday";
             default -> day = "ERROR";
 
         }
