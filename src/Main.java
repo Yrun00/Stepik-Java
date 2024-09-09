38c3c4864f43f22958d6dbc03b7abe3f9a414cbd
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-09-09 21:55:23 +0400

    triangle

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,15 @@
 import java.util.Scanner;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        long number = Math.abs(scan.nextLong());
-        int length = Long.toString(number).length();
-        long sum = 0;
-        int i = 0;
-        long delimiter = (long) Math.pow(10, length);
-        do {
-            sum = sum + number / delimiter;
-            number = number % delimiter;
-            delimiter = delimiter / 10;
-            i++;
-        } while (i <= length
-        );
-        System.out.println(length + " " + sum);
+        int number = scan.nextInt();
+        for (int i = number; 0 < i; i--) {
+            for (int a = i; 0 < a; a--) {
+                System.out.print(number);
+            }
+            number--;
+            System.out.println("");
+        }
     }
 }
