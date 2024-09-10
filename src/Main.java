153a8fa4610aec150425b0bc35c815d257aec07f
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-09-10 21:26:45 +0400

    prime number

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,20 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
         int b = scan.nextInt();
-        int number = Integer.MIN_VALUE;
-        int answer = Integer.MIN_VALUE;
-        if (a > b) {
-            int c = a;
-            a = b;
-            b = c;
+        if (b == 1) {
+            System.out.print("NO");
+            return;
         }
-        for (; a <= b; a++) {
-            int length = Integer.toString(Math.abs(a)).length();
-            int temporary = Math.abs(a);
-            int sum = 0;
-            for (int i = 1; i <= length; i++) {
-                sum = (sum + temporary / (int) Math.pow(10, (length - i)));
-                temporary = temporary % (int) Math.pow(10, (length - i));
-                if (sum > number) {
-                    answer = a;
-                    number = sum;
-                }
-            }
+        BigInteger a = new BigInteger(String.valueOf(b));
+        boolean result = a.isProbablePrime(1);
+        if (result) {
+            System.out.print("YES");
+        } else {
+            System.out.print("NO");
         }
-        System.out.print(answer);
     }
 }
