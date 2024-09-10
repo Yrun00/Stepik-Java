cd43904064a54781cfb0798dfb97fdc525d7b6c5
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-09-10 22:07:13 +0400

    fist composite number from range

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,20 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         long a = scan.nextInt();
         long b = scan.nextInt();
+        if (a > b) {
+            long c = a;
+            a = b;
+            b = c;
+        }
         for (; a <= b; a++) {
             BigInteger c = new BigInteger(String.valueOf(a));
             boolean result = c.isProbablePrime(10);
-            if (result) {
-                System.out.print(a + " ");
+            if (!result) {
+                if (a == 1) continue;
+                System.out.print(a);
+                return;
             }
         }
+        System.out.print("NO");
     }
 }
