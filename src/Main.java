7e783748f8b0380c3c7963fe4a3184946ee2d78c
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-09-10 22:46:48 +0400

    sum of even divisors of numbers in range

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         long a = scan.nextInt();
+        long b = scan.nextInt();
         long sum = 0;
-        for (long i = 2; i < a; i = i + 2) {
-            if (a % i == 0) {
-                sum = sum + i;
+        for (; a <= b; a++) {
+            for (long i = 2; i <= a; i = i + 1) {
+                if (a % i == 0) {
+                    sum = sum + i;
+                    break;
+                }
             }
         }
         System.out.print(sum);
