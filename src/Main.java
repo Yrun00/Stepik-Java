a2b4259477cb3d1aee754392804d524883f487e2
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-09-10 21:53:52 +0400

    range of prime numbers

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int b = scan.nextInt();
-        if (b == 1) {
-            System.out.print("NO");
-            return;
-        }
-        BigInteger a = new BigInteger(String.valueOf(b));
-        boolean result = a.isProbablePrime(1);
-        if (result) {
-            System.out.print("YES");
-        } else {
-            System.out.print("NO");
+        long a = scan.nextInt();
+        long b = scan.nextInt();
+        for (; a < b; a++) {
+            BigInteger c = new BigInteger(String.valueOf(a));
+            boolean result = c.isProbablePrime(1);
+            if (result) {
+                System.out.print(a + " ");
+            }
         }
     }
 }
