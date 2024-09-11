4d6440b937cfeb34b08627c6eb41b59ecdbd2c75
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-09-12 01:06:50 +0400

    average() overload

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,25 @@
 import java.math.BigInteger;
 import java.util.Scanner;
 
-public class Main {
-
+class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
-        double a = scan.nextDouble();
-        double result;
-        if (number == 2) {
-            double b = scan.nextDouble();
-            result = square(a, b);
-        } else result = square(a);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        double x = scan.nextDouble();
+        double y = scan.nextDouble();
+        double z = scan.nextDouble();
+        System.out.printf("%.2f %.2f\n", average(a, b), average(a, b, c));
+        System.out.printf("%.2f %.2f\n", average(x, y), average(x, y, z));
+    }
 
-        System.out.printf("%5.2f\n", result);
+    static double average(double a, double b) {
+        return (a + b) / 2.0d;
+    }
+
+    static double average(double a, double b, double c) {
+        return (a + b + c) / 3.0d;
     }
 
     static double square(double a) {
