d5b639ca7f48856d078fb344702c67405ae9ee56
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-09-12 20:43:56 +0400

    array, > to average

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,24 @@ class Main {
         int arrayLength = scan.nextInt();
         int numberForRandom = scan.nextInt();
         Random rand = new Random(numberForRandom);
-        int[] list = new int[arrayLength];
-        int sum = 0;
-        double composition = 1;
+        double[] list = new double[arrayLength];
+        double sum = 0;
         for (int i = 0; i < arrayLength; i++) {
-            int a = rand.nextInt(-5, 6);
+            double a = rand.nextDouble(0, 5);
             list[i] = a;
-            System.out.print(a + " ");
-            if (a >= 0) {
-                sum = sum + a;
-            } else {
-                composition = composition * a;
-            }
+            System.out.printf("%.2f", a);
+            System.out.print(" ");
+            sum = sum + a;
         }
         System.out.println();
-        System.out.print(sum + " " + composition);
+        double average = sum / arrayLength;
+        System.out.printf("%.2f\n", average);
+        for (int i = 0; i < arrayLength; i++) {
+            if (list[i] > average) {
+                list[i] = average;
+            }
+            System.out.printf("%.2f", list[i]);
+            System.out.print(" ");
+        }
     }
 }
